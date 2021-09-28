c83eb497eace4ab1df5bec1fc2a80b904906c89d
Bhargav <bhargav.kulkarni1@gmail.com>
2021-09-29 00:18:25 +0530

    question 5.1

--- tutorial-5/question-1/sender.c
+++ tutorial-5/question-1/sender.c
@@ -2,16 +2,22 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<string.h>
+#include<stdbool.h>
 
 int main() {
     char buffer[256];
-    
-    printf("Enter message:\n");
-    fgets(buffer, 256, stdin);
+    char otp_buffer[256];
 
-    int fd = open("fifo_to", O_WRONLY);
-    write(fd, buffer, 256);
-    printf("[%d]Sent: %s\n", getpid(), buffer);
-    close(fd);
+    int fd_out = open("fifo_to", O_WRONLY);
+    int fd_in = open("fifo_from", O_RDONLY);
+    
+    printf("[%d] Enter message: ", getpid());
+    fgets(buffer, 256, stdin);
+    write(fd_out, buffer, 256);
+    int n = read(fd_in, otp_buffer, 256);
+    printf("[%d] Recieved flipped message: %s\n", getpid(), otp_buffer);
+
+    close(fd_in);
+    close(fd_out);
     return 0;
 }
--- tutorial-5/question-1/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    char buffer[100];
-    fgets(buffer, 100, stdin);
-    printf("> %s\n", buffer);
-}
